2ddae1c3e34ec9534008a0bdc84d75bf80dcf7d9
Brad Jolicoeur <brad@jolicoeurs.net>
2018-07-27 08:14:39 -0400

    simple example working

--- pcf-console-demo/Program.cs
+++ pcf-console-demo/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace pcf_console_demo
 {
     class Program
     {
+        // AutoResetEvent to signal when to exit the application.
+        private static readonly AutoResetEvent waitHandle = new AutoResetEvent(false);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Fire and forget
+            Task.Run(() =>
+            {
+                var random = new Random(10);
+                while (true)
+                {
+                    // Write here whatever your side car applications needs to do.
+                    // In this sample we are just writing a random number to the Console (stdout)
+                    Console.WriteLine($"Loop = {random.Next()}");
+
+                    // Sleep as long as you need.
+                    Thread.Sleep(1000);
+                }
+            });
+
+            // Handle Control+C or Control+Break
+            Console.CancelKeyPress += (o, e) =>
+            {
+                Console.WriteLine("Exit");
+
+                // Allow the manin thread to continue and exit...
+                waitHandle.Set();
+            };
+
+            // Wait
+            waitHandle.WaitOne();
         }
     }
 }
